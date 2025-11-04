91c8e821b7863b25988ba2ddf36a2d971bbfdc4b
max1dka <mitrofanov.m00@icloud.com>
2025-11-04 13:26:00 +0300

    + BOX, update

--- src/main/java/com/stepup/projMainApplication.java
+++ src/main/java/com/stepup/projMainApplication.java
@@ -4,5 +4,11 @@ public class projMainApplication {
     public static void main (String[] args) {
         System.out.println("Hello World!");
         System.out.println("111111");
+        System.out.println("111221");
+        System.out.println("1112331");
+    }
+
+    public static void doSomething() {
+        System.out.println(1);
     }
 }
