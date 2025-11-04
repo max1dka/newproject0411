5a509c43716a46a51e114e766b61e7a192d276bb
max1dka <mitrofanov.m00@icloud.com>
2025-11-04 13:18:34 +0300

    Доработка логики

--- src/main/java/com/stepup/projMainApplication.java
+++ src/main/java/com/stepup/projMainApplication.java
@@ -3,5 +3,6 @@ package com.stepup;
 public class projMainApplication {
     public static void main (String[] args) {
         System.out.println("Hello World!");
+        System.out.println("111111");
     }
 }
